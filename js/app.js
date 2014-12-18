7333034d59ba1a7e1c398003c525aab8729a40c8
Cheryl Court <cheryl.court@gmail.com>
2014-12-17 22:22:13 -0700

    Updated app.js and index.html to get things in a state where the background and player character are drawn on the screen.

--- js/app.js
+++ js/app.js
@@ -24,13 +24,29 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  //put attributes here
+    this.sprite = 'images/char-boy.png';
+    this.x = 150;
+    this.y = 405;
+}
 
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+}
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+allEnemies = [];
+player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
