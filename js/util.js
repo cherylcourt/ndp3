d0b0280f4ec525ecc5d34c255b49d914259e7888
Cheryl Court <cheryl.court@gmail.com>
2014-12-19 16:07:20 -0700

    Added comment header to inheritsFrom method.

--- js/util.js
+++ js/util.js
@@ -1,4 +1,15 @@
-
+/**
+ * This function contains the code necessary for subclassing and should be called
+ * immediately following the creation of a class constructor for a class that wants
+ * to be a subclass of another and inherit its properties and methods
+ *
+ * e.g.
+ * ANewSubClass = function() {}
+ * ANewSubClass.inheritsFrom(ExistingClass)
+ *
+ * @param the superclass this class wants to inherit from
+ * @return the subclass
+ */
 Function.prototype.inheritsFrom = function( parentClassOrObject ){ 
 	if ( parentClassOrObject.constructor == Function ) 
 	{ 
