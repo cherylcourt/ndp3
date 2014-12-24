c02e7048909db7fdf64842cb38e16bfc83195eb4
Cheryl Court <cheryl.court@gmail.com>
2014-12-23 21:37:38 -0700

    Moved Item function definitions to Item prototype; terminated function definitions with semi-colons

--- js/app.js
+++ js/app.js
@@ -8,31 +8,31 @@ var Item = function(sprite, x, y, width, verticalBuffer) {
     this.y = y; // Item's current y-position
     this.halfVisibleWidth = width / 2;
     this.rowAdjust = verticalBuffer;
+};
 
-    this.midPoint = function() {
-        return this.x + 50.1;
-    }
+Item.prototype.midPoint = function() {
+    return this.x + 50.5;
+};
 
-    this.visibleLeft = function() {
-        return this.midPoint() - this.halfVisibleWidth;
-    }
+Item.prototype.visibleLeft = function() {
+    return this.midPoint() - this.halfVisibleWidth;
+};
 
-    this.visibleRight = function() {
-        return this.midPoint() + this.halfVisibleWidth;
-    }
+Item.prototype.visibleRight = function() {
+    return this.midPoint() + this.halfVisibleWidth;
+};
 
-    this.onRow = function() {
-        return (this.y - this.rowAdjust)/83;
-    }
+Item.prototype.onRow = function() {
+    return (this.y - this.rowAdjust)/83;
+};
 
-    this.onColumn = function() {
-        return(this.x / 101);
-    }
-}
+Item.prototype.onColumn = function() {
+    return(this.x / 101);
+};
 
 Item.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 Item.prototype.collidingWith = function(item) {
     if(this.onRow() == item.onRow()) {
@@ -43,29 +43,29 @@ Item.prototype.collidingWith = function(item) {
             (item.visibleRight() < this.visibleRight() &&
             item.visibleRight() > this.visibleLeft());
     }
-}
+};
 
 Item.prototype.resetPosition = function() {
     this.x = this.startingXPosition;
     this.y = this.startingYPosition;
-}
+};
 
 var Enemy = function() {
     this.verticalBuffer = 57;
 
     this.generateYPosition = function() {
         return Math.floor(Math.random() * 3) * 83 + this.verticalBuffer;
-    }
+    };
 
     Item.call(this,
         'images/enemy-bug-red.png',
         -102,
         this.generateYPosition(),
         86,
-        this.verticalBuffer)
+        this.verticalBuffer);
 
     this.setSpeed();
-}
+};
 
 Enemy.inheritsFrom(Item);
 
@@ -80,12 +80,12 @@ Enemy.prototype.update = function(dt) {
     // any movement is multiplied by the dt parameter to ensure the game
     // runs at the same speed for all computers.
     this.x += this.speed * dt;
-}
+};
 
 Enemy.prototype.reset = function() {
     this.resetPosition();
     this.setSpeed();
-}
+};
 
 /**
  * Sets the speed of the enemy to a random value between 100 and 599 and
@@ -94,7 +94,7 @@ Enemy.prototype.reset = function() {
 Enemy.prototype.setSpeed = function () {
     this.speed= Math.floor((Math.random() * 500) + 100);
     this.setSpriteBySpeed();
-}
+};
 
 /**
  * Sets the sprite image based on the speed of the enemy
@@ -117,16 +117,16 @@ Enemy.prototype.setSpriteBySpeed = function () {
     } else if (this.speed >= 100) {
         this.sprite = 'images/enemy-bug-blue.png';
     }
-}
+};
 
 var Player = function() {
-    Item.call(this, 'images/char-boy.png', 202, 380, 36, 48)
+    Item.call(this, 'images/char-boy.png', 202, 380, 31, 48);
     this.verticalMove = 83;
     this.horizontalMove = 101;
     this.collideSound = new Audio('sounds/crunch.wav');
     this.splashSound = new Audio('sounds/water-splash.wav');
     this.resetWalkingArray();
-}
+};
 
 Player.inheritsFrom(Item);
 
@@ -158,19 +158,19 @@ Player.prototype.update = function() {
         GameProperties.currentGamePoints += 100;
         player.resetWalkingArray();
     }
-}
+};
 
 Player.prototype.reset = function() {
     this.resetWalkingArray();
     this.resetPosition();
-}
+};
 
 Player.prototype.resetWalkingArray = function() {
     this.walkedSuccess = [];
     for(var i = 0; i < 3; i++) {
         this.walkedSuccess.push([]);
     }
-}
+};
 
 Player.prototype.handleInput = function(input) {
     switch (input) {
@@ -193,23 +193,23 @@ Player.prototype.handleInput = function(input) {
             this.walkedSuccess[this.onRow()].push(this.onColumn());
         }
     }
-}
+};
 
 Player.prototype.moveLeft = function() {
     if(this.x >= this.horizontalMove) {
         this.x -= this.horizontalMove;
     }
-}
+};
 
 Player.prototype.moveRight = function() {
     if(this.x + this.horizontalMove < canvas.width) {
         this.x += this.horizontalMove;
     }
-}
+};
 
 Player.prototype.hasReachedTopRow = function() {
     return this.y <= this.verticalMove;
-}
+};
 
 Player.prototype.moveUp = function() {
 
@@ -228,17 +228,17 @@ Player.prototype.moveUp = function() {
     } else {
         this.y -= this.verticalMove;
     }
-}
+};
 
 Player.prototype.moveDown = function() {
     if(this.y < this.startingYPosition) {
         this.y += this.verticalMove;
     }
-}
+};
 
 Player.prototype.setCharacter = function(sprite) {
     this.sprite = sprite;
-}
+};
 
 /**
  * Creates a new PauseScreen class.  This class contains all the information necessary to display the pause screen
@@ -257,7 +257,7 @@ var PauseScreen = function() {
 
     this.characterSelection = 0;
     this.colouredTileModeOn = false;
-}
+};
 
 /**
  * This is called to draw the pause screen on the canvas.
@@ -273,7 +273,7 @@ PauseScreen.prototype.render = function() {
     this.drawGameModeText('images/2-icon.png', 'Collectibles', false, 397);
     this.drawGameModeText('images/3-icon.png', 'Alternate Directions', false, 457);
     this.drawEscapeMessage(555);
-}
+};
 
 /**
  * This method draws the background and border of the pause screen so that the game options are more visible to
@@ -287,7 +287,7 @@ PauseScreen.prototype.renderOverlay = function() {
     ctx.strokeStyle = 'white';
     ctx.lineWidth = 2;
     ctx.strokeRect(10, 60, 485, 516);
-}
+};
 
 /**
  * Draw a grey centered title on the canvas with a black shadow.
@@ -303,7 +303,7 @@ PauseScreen.prototype.drawTitle = function(title, x, y) {
     ctx.fillText(title, x+3, y+3);
     ctx.fillStyle = 'grey';
     ctx.fillText(title, x, y);
-}
+};
 
 /**
  * Draw the characters available for selection.
@@ -317,7 +317,7 @@ PauseScreen.prototype.drawCharacterSelect = function(x, y, spacingInterval) {
     for(var characterIndex in this.characterImages) {
         ctx.drawImage(Resources.get(this.characterImages[characterIndex]), characterIndex * spacingInterval + x, y);
     }
-}
+};
 
 /**
  * Draw the available game mode text as well as whether the game mode is currently "ON" or "OFF"
@@ -344,7 +344,7 @@ PauseScreen.prototype.drawGameModeText = function(image, modeText, isOn, y) {
 
     ctx.drawImage(Resources.get(image), 30, y);
     ctx.fillText(gameModeText, 100, y+40);
-}
+};
 
 /**
  * Draws the message that lets the user know to press the escape key to exit the pause screen.
@@ -356,7 +356,7 @@ PauseScreen.prototype.drawEscapeMessage = function(y) {
     ctx.textAlign = 'center';
     ctx.fillText('Press      to play game', canvas.width/2, y);
     ctx.drawImage(Resources.get('images/esc-icon.png'), canvas.width/2 - 72, y-34);
-}
+};
 
 /**
  * This function is called by the event listener that is listening for keyUp events when the game is
@@ -383,7 +383,7 @@ PauseScreen.prototype.handleInput = function (input) {
             player.resetWalkingArray();
             break;
     }
-}
+};
 
 /**
  * Gets the URL of the image for the selected character
@@ -391,7 +391,7 @@ PauseScreen.prototype.handleInput = function (input) {
  */
 PauseScreen.prototype.getSelectedCharacterImageURL = function() {
     return this.characterImages[this.characterSelection];
-}
+};
 
 allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy()];
 player = new Player();
